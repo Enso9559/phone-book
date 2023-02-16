ec584b39a1435128479232847f7833ee58513b1a
Enso9559 <pavelstrelnikov9559@gmail.com>
2023-02-16 16:04:32 +0200

    page template

---
+++ src/Container/container.js
@@ -0,0 +1,11 @@
+import "./container.css";
+
+const Container = () => {
+  return (
+  <div className="add-container">
+    Container
+    </div>
+  )
+};
+
+export default Container;
---
+++ src/Navigation/navigation.js
@@ -0,0 +1,11 @@
+import "./navigation.css";
+
+const Navigation = () => {
+  return (
+  <div className="add-navigation">
+    Navigation
+    </div>
+  )
+};
+
+export default Navigation;
